9f866d626893d33c76b03e61ca458c84c85bb9d0
unknown <martin2844@gmail.com>
2020-08-15 15:54:15 -0300

    initial scaffold

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const path = require('path');
 const forceSsl = require('force-ssl-heroku');
 
 //First connect to mongoose. 
-
 connectDB();
 
 
@@ -16,18 +15,14 @@ const app = express();
 app.use(forceSsl);
 app.use(cors());
 
-
 //Body Parser
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({extended: false}));
-
-console.log('check');
 
 //Search route
 app.use('/api/products', require('./routes/products'));
 //Call back URL route.
 app.use('/api/results', require('./routes/results'));
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`server started on ${PORT}`));
